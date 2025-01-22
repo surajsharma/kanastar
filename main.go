8afe735098dead35110ec5068b55fd85ed5bcffc
surajsharma <suraj@evenzero.in>
2025-01-22 22:06:40 +0530

    manager

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"github.com/surajsharma/orcs/manager"
 	"github.com/surajsharma/orcs/task"
 	"github.com/surajsharma/orcs/worker"
 )
@@ -28,10 +29,47 @@ func main() {
 	api := worker.Api{Address: host, Port: port, Worker: &w}
 
 	go w.CollectStats()
-
 	go runTasks(&w)
 
-	api.Start()
+	go api.Start()
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+
+		m.AddTask(te)
+		m.SendWork()
+	}
+
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep(15 * time.Second)
+		}
+	}
+
 }
 
 func runTasks(w *worker.Worker) {

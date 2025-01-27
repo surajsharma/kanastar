2cf23de43dd7d612574a2425b8d8642304205210
surajsharma <suraj@evenzero.in>
2025-01-28 00:54:08 +0530

    updated readme

--- main.go
+++ main.go
@@ -20,27 +20,31 @@ func main() {
 	whost := os.Getenv("WORKER_HOST")
 	wport, _ := strconv.Atoi(os.Getenv("WORKER_PORT"))
 
-	fmt.Println("Starting Cube worker")
+	fmt.Println("⏳ Starting Cube worker...")
+
+	// why start a single worker when manager has all of them?
+	// because the manager needs at least one, see ln 41
 
 	w := worker.Worker{
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
 	}
+
 	wapi := worker.Api{Address: whost, Port: wport, Worker: &w}
 
 	go w.RunTasks()
 	go w.CollectStats()
 	go wapi.Start()
 
-	fmt.Println("Starting Cube manager")
+	fmt.Println("⏳ Starting Cube manager...")
 
 	workers := []string{fmt.Sprintf("%s:%d", whost, wport)}
 	m := manager.New(workers)
+
 	mapi := manager.Api{Address: mhost, Port: mport, Manager: m}
 
 	go m.ProcessTasks()
 	go m.UpdateTasks()
 
-	mapi.Start()
-
+	mapi.Start() //this cannot be a goroutine for http.ListenAndServe is blocking
 }

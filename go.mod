586cba2e8893b3918ee97026ebf34061572767bc
surajsharma <suraj@evenzero.in>
2025-02-18 07:53:00 +0530

    init cobra cli

--- go.mod
+++ go.mod
@@ -21,12 +21,15 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/term v0.5.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/pflag v1.0.6 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0 // indirect
 	go.opentelemetry.io/otel v1.34.0 // indirect
--- main.go
+++ main.go
@@ -1,62 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-
-	"github.com/surajsharma/kanastar/manager"
-	"github.com/surajsharma/kanastar/worker"
-)
+import "github.com/surajsharma/kanastar/cmd"
 
 func main() {
-
-	mhost := os.Getenv("MANAGER_HOST")
-	mport, _ := strconv.Atoi(os.Getenv("MANAGER_PORT"))
-
-	whost := os.Getenv("WORKER_HOST")
-	wport, _ := strconv.Atoi(os.Getenv("WORKER_PORT"))
-
-	fmt.Println("⏳ Starting worker...")
-
-	w1 := worker.New("w1", "persistent")
-	w2 := worker.New("w2", "persistent")
-	w3 := worker.New("w3", "persistent")
-
-	wapi1 := worker.Api{Address: whost, Port: wport, Worker: w1}
-	wapi2 := worker.Api{Address: whost, Port: wport + 1, Worker: w2}
-	wapi3 := worker.Api{Address: whost, Port: wport + 2, Worker: w3}
-
-	go w1.RunTasks()
-	go w1.CollectStats()
-	go w1.UpdateTasks()
-	go wapi1.Start()
-
-	go w2.RunTasks()
-	go w2.CollectStats()
-	go w2.UpdateTasks()
-	go wapi2.Start()
-
-	go w3.RunTasks()
-	go w3.CollectStats()
-	go w3.UpdateTasks()
-	go wapi3.Start()
-
-	fmt.Println("⏳ Starting manager...")
-
-	workers := []string{
-		fmt.Sprintf("%s:%d", whost, wport),
-		fmt.Sprintf("%s:%d", whost, wport+1),
-		fmt.Sprintf("%s:%d", whost, wport+2),
-	}
-
-	m := manager.New(workers, "epvm", "persistent")
-
-	mapi := manager.Api{Address: mhost, Port: mport, Manager: m}
-
-	go m.ProcessTasks()
-	go m.UpdateTasks()
-	go m.DoHealthChecks()
-
-	mapi.Start() //this cannot be a goroutine for http.ListenAndServe is blocking
+	cmd.Execute()
 }
